259a89671c91561977bd5e739f0f9a4009e6985a
gehad <gehadgamalazzam@gmail.com>
2023-11-27 16:44:31 +0200

    learn

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,22 @@
 #include "lists.h"
+/**
+*check_cycle - check the list if have a cycle
+*
+*@list: pointer to the list
+*
+*Return: 0 or 1
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *slow = list, *fast = list;
+	while (slow && fast && fast->next)
+{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+{
+			return (1);
+}
+}
+	return (0);
+}
