44cc80471e8b4c737c855bb35e03bbbae27584c9
gehad <gehadgamalazzam@gmail.com>
2023-11-28 20:59:14 +0200

    learn

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+for alpha in range(122, 96, -1):
+    case = 'lower' if alpha % 2 == 1 else 'upper'
+    print("{}".format(
+        chr(alpha).upper() if case == 'lower' else chr(alpha)), end='')
