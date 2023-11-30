6f2207b8a5607ee364c874a1467b63242539b6ba
gehad <gehadgamalazzam@gmail.com>
2023-11-30 12:52:54 +0200

    learn

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the sum of 1 and 2"""
-import sys
-argc = len(sys.argv) - 1
-arguments = sys.argv[1:]
-if argc == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(argc))
-    for item in range(argc):
-        print("{}: {}".format(item+1, arguments[item]))
+    import sys
+    argc = len(sys.argv) - 1
+    arguments = sys.argv[1:]
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(argc))
+        for item in range(argc):
+            print("{}: {}".format(item+1, arguments[item]))
