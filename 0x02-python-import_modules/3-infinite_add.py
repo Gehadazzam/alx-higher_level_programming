4f17c8af860a29846fbf3cb8a4c2a8b3abeefae5
gehad <gehadgamalazzam@gmail.com>
2023-11-30 13:38:22 +0200

    learn

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the sum of 1 and 2"""
-import sys
-argc = len(sys.argv) - 1
-total = 0
-if argc == 0:
-    print(0)
-else:
-    for item in range(1, argc + 1):
-        arguments = int(sys.argv[item])
-        total += arguments
-    print("{}".format(total))
+    """print the addatin of the arguments"""
+    import sys
+    argc = len(sys.argv) - 1
+    total = 0
+    if argc == 0:
+        print(0)
+    else:
+        for item in range(1, argc + 1):
+            arguments = int(sys.argv[item])
+            total += arguments
+        print("{}".format(total))
