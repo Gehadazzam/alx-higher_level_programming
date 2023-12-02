753ab5bb690e77a965a30ff72d6e24116e68e94b
gehad <gehadgamalazzam@gmail.com>
2023-12-02 11:17:21 +0200

    learn

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return(0, None)
+    length = len(sentence)
+    if (length == 0):
+        new_tuple = (length, None)
     else:
-        return(len(sentence), sentence[0])
+        new_tuple = (length, sentence[0])
+    return (new_tuple)
