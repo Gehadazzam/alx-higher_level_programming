876d12f3f35354696d3f1a179a513a6b688e74a1
gehad <gehadgamalazzam@gmail.com>
2023-12-18 16:08:08 +0200

    exexptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as error:
-        print(f"Exception: {error}", sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(error))
         return False
