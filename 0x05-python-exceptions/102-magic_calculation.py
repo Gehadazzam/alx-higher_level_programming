08687255419e0c28c8e10f733908c823598905a6
gehad <gehadgamalazzam@gmail.com>
2023-12-18 20:16:41 +0200

    exexptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
+    calc = 0
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception("Too far")
+            else:
+                calc += (a ** b) / x
+        except:
+            calc = b + a
+            pass
+    return calc
