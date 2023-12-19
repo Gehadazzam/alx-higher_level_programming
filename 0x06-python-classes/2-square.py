db3d0ad16eff6febeb74d29c06d5e5f9c45c3b99
gehad <gehadgamalazzam@gmail.com>
2023-12-19 15:38:12 +0200

    yah python

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+creat new class
+named Square
+"""
+
+
+class Square:
+    """define private attribuite"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
