af5284814ac25f6a42c416ea928f4f64fc568311
gehad <gehadgamalazzam@gmail.com>
2023-12-19 16:10:35 +0200

    yah python

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,4 +48,4 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print("#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
