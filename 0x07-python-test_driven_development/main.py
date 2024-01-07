5f13bd1141d320e581b7faeaa08ad22bdb1bd8b9
gehad <gehadgamalazzam@gmail.com>
2024-01-07 20:22:22 +0200

    update my tests

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,6 +1,5 @@
-##!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
+#!/usr/bin/python3
+lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[7, 2]], [[9, 4], [8, 5]]))
-print(matrix_mul([[8, 7], [3, 7]], [[7, 3, 1],[5, 6, 2]]))
+print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(lazy_matrix_mul([[8, 7], [3, 7]], [[7, 3, 1],[5, 6, 2]]))
