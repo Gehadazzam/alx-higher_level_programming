26894cb8a8b7e20b3285cc31a51cbe4253833017
gehad <gehadgamalazzam@gmail.com>
2024-01-08 14:45:45 +0200

    crat base class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+creat a base geo class
+"""
+
+
+class BaseGeometry:
+    """init the class"""
+
+    def area(self):
+        """ just type massage"""
+        raise Exception("area() is not implemented")
