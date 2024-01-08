8725f7ee20219d6846304fc77411e390e0e784f5
gehad <gehadgamalazzam@gmail.com>
2024-01-08 14:51:28 +0200

    creat basse class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,11 +9,13 @@ class BaseGeometry:
 
     def area(self):
         """ just type massage"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """ validate a value"""
+
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
