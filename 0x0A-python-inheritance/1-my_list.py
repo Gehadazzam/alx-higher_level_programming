0b0c3a587e68048608ca81d493442020a1a96f30
gehad <gehadgamalazzam@gmail.com>
2024-01-08 13:23:13 +0200

    creat the list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+creat class my list and it's base case list
+"""
+
+
+class MyList(list):
+    """
+    creat a subclass
+    """
+
+    def print_sorted(self):
+        """
+        print the list sorted
+        """
+        print(sorted(self))
