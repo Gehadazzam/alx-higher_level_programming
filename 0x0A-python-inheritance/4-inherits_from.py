db9cfb6c61f0a426ec91075d6288205a21fa3ffb
gehad <gehadgamalazzam@gmail.com>
2024-01-08 14:36:06 +0200

    check class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+check if object is an instance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    return true or false
+    """
+    return False if type(obj) is a_class else isinstance(obj, a_class)
