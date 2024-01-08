aac2d6b4dd3e5833314c1c16fe55d326c63fae1c
gehad <gehadgamalazzam@gmail.com>
2024-01-08 18:17:54 +0200

    int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+new class inherits from int
+"""
+
+
+class MyInt(int):
+    """My int class invert the true and false"""
+
+    def __eq__(self, other):
+        """True"""
+
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """false"""
+
+        return super().__eq__(other)
