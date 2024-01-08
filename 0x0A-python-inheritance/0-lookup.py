480a72f9ce37c5d65f2ee53012ae5e3a52202401
gehad <gehadgamalazzam@gmail.com>
2024-01-08 13:14:00 +0200

    look up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module to get the atrribuites
+"""
+
+
 def lookup(obj):
+    """
+    return a list of attributes and methodes of a class
+    """
     return [attribute for attribute in dir(obj)]
