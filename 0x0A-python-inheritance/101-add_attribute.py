46cf3b8fda7e51b15fd91cc6663944a8399a1ad5
gehad <gehadgamalazzam@gmail.com>
2024-01-08 18:23:21 +0200

    attr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module to add attribute
+"""
+
+
+def add_attribute(obj, attr, value):
+    """raise an error if we can add new one"""
+    if isinstance(obj, (int, float, str, bool, type(None))):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr, value)
