bdf1ba423060699bd217a8ff262072731a920e40
gehad <gehadgamalazzam@gmail.com>
2024-01-09 13:31:04 +0200

    load

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """useing with and json to load from a file"""
 
     with open(filename, "r", encoding='utf-8') as myfile:
-        json.load(myfile)
+        json.load(myfile.read())
