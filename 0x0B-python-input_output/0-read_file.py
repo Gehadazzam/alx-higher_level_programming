068e70c15a2cfa65bf475c55dbd3e6ed9a4c2e5b
gehad <gehadgamalazzam@gmail.com>
2024-01-09 11:16:30 +0200

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ Module to read from a file and print it
 
 def read_file(filename=""):
     """print frm a file"""
-    if filename:
-        with open(filename, 'r', encoding='utf-8') as my_file:
-            print(my_file.read())
+    with open(filename, 'r', encoding='utf-8') as my_file:
+        text = my_file.read()
+        print(text, end="")
