9b1dab67334806d4e5cacf9d0f58efbd6076cd1c
gehad <gehadgamalazzam@gmail.com>
2024-01-09 13:24:00 +0200

    write with json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""write to file importing json"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """useing with and json to write to a file"""
+
+    with open(filename, "w", encoding='utf-8') as myfile:
+        json.dump(my_obj, myfile)
