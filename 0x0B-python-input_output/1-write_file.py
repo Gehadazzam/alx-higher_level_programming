4931ef2d74bc10bf8b87852f845d66bb79eda91c
gehad <gehadgamalazzam@gmail.com>
2024-01-09 12:53:57 +0200

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ Module to write in a file
 def write_file(filename="", text=""):
     """creat the file if it is not there"""
 
-    with open(filename, "r", encoding="utf-8") as myfile:
+    with open(filename, "r") as myfile:
         return myfile.write(text)
