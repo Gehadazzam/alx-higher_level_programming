d5d428bc5d76265ede2940261b0cdbfb710ecac2
gehad <gehadgamalazzam@gmail.com>
2024-01-09 18:20:51 +0200

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+argv = sys.argv[1:]
+
+try:
+    myfile = load_from_json_file("add_item.json")
+
+except FileNotFoundError:
+    myfile = []
+
+myfile.extend(argv)
+save_to_json_file(argv, "add_item.json")
