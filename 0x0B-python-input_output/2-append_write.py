a07e0c4735a1f498c2ebf6545602dae10a8b663a
gehad <gehadgamalazzam@gmail.com>
2024-01-09 13:02:40 +0200

    appened

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Module to appened a file
+"""
+
+
+def append_write(filename="", text=""):
+    """creat the file if it is not there"""
+
+    with open(filename, "a", encoding="utf-8") as myfile:
+        return myfile.write(text)
