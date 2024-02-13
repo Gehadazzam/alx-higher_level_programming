7541e547245d9bb4a88f17bd46db3880e46bd1a8
gehad <gehadgamalazzam@gmail.com>
2024-02-14 01:20:33 +0200

    query

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- creates the database hbtn_0c_0 in your MySQL server. If the database hbtn_0c_0 already exists not fail
+CREATE DATABASES   IF NOT EXITSTS 'hbtn_0c_0';
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes the database hbtn_0c_0 in your MySQL server If the database hbtn_0c_0 doesn’t exist, your script should not fail
+DELETE DATABASES IF EXISTS 'hbtn_0c_0';
