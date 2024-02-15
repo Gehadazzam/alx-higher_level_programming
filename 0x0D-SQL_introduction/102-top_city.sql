30dc30c0b36b6502e26fb604496e0c738b62a161
gehad <gehadgamalazzam@gmail.com>
2024-02-15 23:02:38 +0200

    tempreture

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,5 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE MONTH = 7 OR MONTH = 8
+GROUP bY city ORDER BY avg_temp DESC LIMIT 3;
