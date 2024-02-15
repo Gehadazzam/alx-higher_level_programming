c83c043f3cf92a82e02b3f5b085cb8c1797e17e9
gehad <gehadgamalazzam@gmail.com>
2024-02-15 03:32:43 +0200

    tvshows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,8 +3,7 @@
 -- Results must be sorted in descending order by the rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title,
-SUM(tv_shows_rate.rating) AS rating_sum
+SELECT title, SUM(rate) AS rating
 FROM tv_shows
-JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_shows.id, tv_shows.titl ORDER BY rating_sum DESC;
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,3 +3,8 @@
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
+SELECT name, SUM(rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY name ORDER BY rating DESC;
