888ca3878172499ac305529ee5ff3036b8157437
gehad <gehadgamalazzam@gmail.com>
2024-02-15 22:45:57 +0200

    num

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 --  lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-
+SELECT score, COUNT(*) AS num
+FROM second_table
+GROUP BY score ORDER BY num DESC;
