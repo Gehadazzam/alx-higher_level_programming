92c7f6986a4587f41992d9e2891d719113702cf6
gehad <gehadgamalazzam@gmail.com>
2024-03-11 16:31:38 +0200

    more loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const numOcurrence = parseInt(arg);
+if (isNaN(numOcurrence)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < numOcurrence; i++) {
+    console.log('C is fun');
+  }
+}
