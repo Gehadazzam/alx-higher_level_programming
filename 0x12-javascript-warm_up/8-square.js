76f04fdcf74f2899c79509cb6e54d79bd54de445
gehad <gehadgamalazzam@gmail.com>
2024-03-11 16:40:00 +0200

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const size = parseInt(arg);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let width = 0; width < size; width++) {
+    for (let height = 0; height < size; height++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+}
