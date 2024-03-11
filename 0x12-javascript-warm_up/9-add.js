a1756d93cff5785e9751f4387f2cace20e1eb31d
gehad <gehadgamalazzam@gmail.com>
2024-03-11 16:47:49 +0200

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+
+const firstArg = process.argv[2];
+const seconedArg = process.argv[3];
+const firstNum = parseInt(firstArg);
+const seconedNum = parseInt(seconedArg);
+if (isNaN(firstNum) || isNaN(seconedNum)) {
+  console.log('NaN');
+} else {
+  add(firstNum, seconedNum);
+}
