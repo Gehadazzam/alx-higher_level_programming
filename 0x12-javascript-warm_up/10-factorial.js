21253ecceffa7ccd6a159d50f0bdd74c44570bc8
gehad <gehadgamalazzam@gmail.com>
2024-03-11 23:27:45 +0200

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+function getFactorial (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else if (num > 1) {
+    return num * getFactorial(num - 1);
+  }
+}
+const args = process.argv[2];
+const number = parseInt(args);
+if (isNaN(number)) {
+  console.log('1');
+} else {
+  console.log(getFactorial(number));
+}
