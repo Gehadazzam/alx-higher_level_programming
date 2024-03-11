78577da1127db4738a1cf7c7b94204caeaa59533
gehad <gehadgamalazzam@gmail.com>
2024-03-11 23:52:44 +0200

    seconed biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.length;
+if (args === 1 || args === 2) {
+  console.log('0');
+} else {
+  const num = process.argv.slice(2);
+  const sortNum = num.map(Number).sort((x, z) => z - x);
+  console.log(sortNum[1]);
+}
