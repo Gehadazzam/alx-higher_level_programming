6a1ff7fbc4446258d7f0335d172a57d4fae3c93f
gehad <gehadgamalazzam@gmail.com>
2024-03-12 14:00:58 +0200

    Sqaure

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    let rec = '';
+    for (let n = 0; n < this.height; n++) {
+      for (let idx = 0; idx < this.width; idx++) {
+        rec += c;
+      }
+      rec += '\n';
+    }
+    process.stdout.write(rec);
+  }
+}
+module.exports = Square;
