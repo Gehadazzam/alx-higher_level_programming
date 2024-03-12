0ed6a38f9b80476f6cf5b585bcbd9b527b485121
gehad <gehadgamalazzam@gmail.com>
2024-03-12 15:37:10 +0200

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const reverseList = [];
+  for (let idx = list.length - 1; idx >= 0; idx--) {
+    reverseList.push(list[idx]);
+  }
+  return reverseList;
+};
