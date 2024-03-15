e31221e63f63782cfa64576fd12f11d81e89b0b0
gehad <gehadgamalazzam@gmail.com>
2024-03-15 03:20:39 +0200

    init files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const counterDict = {};
+for (const [id, num] of dict.entries()){
+    if (num in counterDict){
+        counterDict[num].push(id);
+    } else {
+        counterDict[num] = id;
+    }
+}
+console.log(counterDict);
