52ed80a1fa52ec7973fb26a6c6ff73a073aef7d3
gehad <gehadgamalazzam@gmail.com>
2024-03-16 03:48:47 +0200

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+module to handle database using python and MYSQLDB
+"""
+import MySQLdb
+import sys
+
+
+def show_states(name, pas, database):
+    """show the state"""
+
+    connect = MySQLdb.connect(
+        host="localhost", user=name, passwd=pas, db=database, port=3306
+    )
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    tab = cursor.fetchall()
+    for row in tab:
+        print(row)
+    cursor.close()
+    connect.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    name = sys.argv[1]
+    pas = sys.argv[2]
+    database = sys.argv[3]
+
+    show_states(name, pas, database)
