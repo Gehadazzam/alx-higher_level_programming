3fe182a699dab90a96d4dee1966ea0468471929e
gehad <gehadgamalazzam@gmail.com>
2024-03-16 15:09:17 +0200

    state by argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+module to handle database using python and MYSQLDB
+"""
+import MySQLdb as MDB
+import sys
+
+
+def showstate(name, pas, database, arg):
+    """show the state that match argument"""
+
+    connect = MDB.connect(
+        host="localhost", user=name, passwd=pas, db=database, port=3306
+    )
+    cursor = connect.cursor()
+    query = "SELECT * FROM states WHERE NAME LIKE BINARY %s ORDER BY id ASC"
+    cursor.execute(query, (arg,))
+    tab = cursor.fetchall()
+    for row in tab:
+        print(row)
+    cursor.close()
+    connect.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    name = sys.argv[1]
+    pas = sys.argv[2]
+    database = sys.argv[3]
+    arg = sys.argv[4]
+
+    showstate(name, pas, database, arg)
