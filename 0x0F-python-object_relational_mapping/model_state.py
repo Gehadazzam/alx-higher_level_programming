c5f6758fd29e901338a96ba96727b5d3c67ccbf1
gehad <gehadgamalazzam@gmail.com>
2024-03-17 16:38:04 +0200

    update model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import create_engine
 Base = declarative_base()
 
 
