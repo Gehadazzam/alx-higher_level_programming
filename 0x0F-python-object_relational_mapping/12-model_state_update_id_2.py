5717dff0f7e574e1433254734c3b473f0f0554fa
gehad <gehadgamalazzam@gmail.com>
2024-03-18 01:30:13 +0200

    more quereis

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""model to list State from data base"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, pas, data),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    update = session.query(State).filter_by(id='2')
+    update.name = 'New Mexico'
+    session.commit()
+    session.close()
