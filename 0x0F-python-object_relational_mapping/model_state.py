20a4e2c31bad1827a036f1c15b54ed033e175b3a
gehad <gehadgamalazzam@gmail.com>
2024-03-17 16:49:07 +0200

    updatemodel

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,11 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 import sys
-
+"""module to manipulate data in python using sqlalchemy"""
 Base = declarative_base()
 
 
 class State(Base):
+    """new class"""
+
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     __name__ = Column(String(128), nullable=False)
