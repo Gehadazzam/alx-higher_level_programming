899748130bbd0885b66dac1995d8bb3851edad51
gehad <gehadgamalazzam@gmail.com>
2024-03-17 20:59:14 +0200

    test model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,8 +4,7 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
-import sys
+
 Base = declarative_base()
 
 
@@ -13,16 +12,5 @@ class State(Base):
     """new class"""
 
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    __name__ = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
