409e04214a6605b2d98bd1813158836358847874
gehad <gehadgamalazzam@gmail.com>
2024-03-17 21:41:47 +0200

    fetch

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""model to list State from data base"""
+
+
+from sqlalchemy import create_engine, sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    if sys.argv != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, pas, data),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    tab = session.query(State).order_by(State.id).all()
+    for row in tab:
+        print(f"{_.id}: {_.name}")
+    session.close()
