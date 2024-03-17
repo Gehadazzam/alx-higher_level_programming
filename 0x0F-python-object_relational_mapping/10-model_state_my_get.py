25732e33989500cd62f92cf2b970bfeb547ff86d
gehad <gehadgamalazzam@gmail.com>
2024-03-18 01:05:13 +0200

    sql name to search

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""model to list State from data base"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+    stateName = sys.argv[4]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, pas, data),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    tab = session.query
+    (State).order_by(State.id).filter(State.name == sys.argv[4]).first()
+    if tab:
+        print(f"{tab.id}")
+    else:
+        print("Not found")
+    session.close()
