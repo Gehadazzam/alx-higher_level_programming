ae86b416eed0bad5c6007f0359248154bb477583
gehad <gehadgamalazzam@gmail.com>
2024-03-17 02:34:35 +0200

    city

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+module to handle database using python and MYSQLDB
+"""
+import MySQLdb as MDB
+import sys
+
+
+def showcities(name, pas, database, arg):
+    """show the cities"""
+
+    connect = MDB.connect(
+        host="localhost", user=name, passwd=pas, db=database, port=3306
+    )
+    cursor = connect.cursor()
+    cursor.execute(
+        """ SELECT cities.name
+        FROM cities JOIN states ON cities.state_id = states.id
+        WHERE states.name = %(n)s
+        ORDER BY cities.id ASC""",
+        {"n": arg},
+    )
+    tab = cursor.fetchall()
+    names = [row[0] for row in tab]
+    print(", ".join(names))
+    cursor.close()
+    connect.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    name = sys.argv[1]
+    pas = sys.argv[2]
+    database = sys.argv[3]
+    arg = sys.argv[4]
+    showcities(name, pas, database, arg)
