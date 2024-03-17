e2b87f07d3e40d77a6bb7ec02d543b63be7655bf
gehad <gehadgamalazzam@gmail.com>
2024-03-18 00:47:07 +0200

    first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""model to list State from data base"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(username, pas, data),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    tab = session.query(State).order_by(State.id).first()
+    if tab:
+        print(f"{tab.id}: {tab.name}")
+    else:
+        print("Nothing")
+    session.close()
