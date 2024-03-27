e5a8a1e1a7f1b76797c1154465494916b4026f05
Gehad azzam <gehadgamalazzam@gmail.com>
2024-03-27 08:26:22 -0700

    more curl

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #sends a GET request to the URL
+curl -sX GET "$1" -L -H "X-School-User-Id: 98" 
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed UR
+curl -sX POST "$1" -L  -d "email=test@gmail.com&subject=I will always be here for PLD"
