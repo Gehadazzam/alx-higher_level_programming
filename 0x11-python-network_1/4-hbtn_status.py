91cba44374f32cf2205f049851725f4b6fd8e48b
gehad <gehadgamalazzam@gmail.com>
2024-03-29 00:59:46 +0200

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-
-
+"""fetch url"""
+import requests as r
 
 if __name__ == "__main__":
+    """Your code should not be executed when imported"""
+    new_request = r.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(new_request.text)))
+    print("\t- content: {}".format(new_request.text))
