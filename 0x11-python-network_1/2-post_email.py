cba6efbd996e050860bd2670e8f0c7c0ebac3cfd
gehad <gehadgamalazzam@gmail.com>
2024-03-29 00:37:58 +0200

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-
-
+"""
+    sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header of the response.
+"""
+import sys
+from urllib import request as ubr
+from urllib.parse import urlencode as enc
 
 if __name__ == "__main__":
+    email = {"email": sys.argv[2]}
+    new_request = ubr.Request(sys.argv[1], enc(email).encode("ascii"))
+    with ubr.urlopen(new_request) as response:
+        print(response.read().decode("utf-8", "replace"))
