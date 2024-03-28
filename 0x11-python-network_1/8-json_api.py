245e2f04bc2875048d915b733bd2625db98a0b53
gehad <gehadgamalazzam@gmail.com>
2024-03-29 01:53:07 +0200

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
-
+"""
+    sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter.
+"""
+from sys import argv as av
+import requests as r
 
 
 if __name__ == "__main__":
+    if len(av) == 1:
+        arg = ""
+    else:
+        arg = av[1]
+    new_request = r.post(
+        "http://0.0.0.0:5000/search_user", {"q", arg})
+    try:
+        response = new_request.json()
+        if response == {}:
+            print("No result")
+        else:
+            print(f"[{response.get("id")}] {response.get("name")}")
+    except ValueError:
+        print("Not a valid JSON")
