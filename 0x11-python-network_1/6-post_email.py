07bd236715b5a0db6a0a1b0aa194e409cb4212bf
gehad <gehadgamalazzam@gmail.com>
2024-03-29 01:14:29 +0200

    post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-
-
+"""
+    sends a POST request to the passed URL with the email as a parameter,
+    and displays the body of the response
+"""
+import sys
+import requests as r
 
 if __name__ == "__main__":
+    email = {"email": sys.argv[2]}
+    new_request = r.post(sys.argv[1], email)
+    print(new_request.text)
