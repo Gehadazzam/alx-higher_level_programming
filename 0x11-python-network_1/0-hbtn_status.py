d0e991f540b4aed1f7cd8df6a7ea59537baa7169
gehad <gehadgamalazzam@gmail.com>
2024-03-28 21:29:45 +0200

    pycode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""fetch url"""
 import urllib.request as ur
 
 if __name__ == "__main__":
+    """Your code should not be executed when imported"""
+
     new_request = ur.Request("https://alx-intranet.hbtn.io/status")
     with ur.urlopen(new_request) as response:
         text = response.read()
