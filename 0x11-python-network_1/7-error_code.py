667919af89f9fb6917d8462fcfecad457b3a3498
gehad <gehadgamalazzam@gmail.com>
2024-03-29 01:17:56 +0200

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-
-
+"""
+     sends a request to the URL and displays the body of the response
+"""
+from sys import argv as av
+import requests as r
 
 if __name__ == "__main__":
+    new_request = r.get(av[1])
+    if new_request.status_code < 400:
+        print(new_request.text)
+    else:
+        print(f"Error code: {new_request.status_code}")
