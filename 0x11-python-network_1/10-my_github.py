356c17a4172e7cce7c573522c7609ed806e13c67
gehad <gehadgamalazzam@gmail.com>
2024-03-29 02:27:12 +0200

    githup

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-
-
+"""
+    Python script that takes your GitHub credentials (username and
+    password) and uses the GitHub API to display your id
+"""
+import requests as r
+from requests.auth import HTTPBasicAuth as ath
+from sys import argv as av
 
 if __name__ == "__main__":
+    checker = ath(av[1], av[2])
+    new_request = r.get("https://api.github.com/user", auth=checker)
+    print(new_request.json().get("id"))
