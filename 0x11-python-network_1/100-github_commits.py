e6e310b27881009e2a2e0e7babc73ef3504f2073
gehad <gehadgamalazzam@gmail.com>
2024-03-29 02:47:15 +0200

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-
+"""
+    takes 2 arguments in order to solve this challenge.
+    The first argument will be the repository name
+    The second argument will be the owner name
+"""
+from sys import argv as av
+import requests as r
 
 
 if __name__ == "__main__":
+    new_request = r.get(
+        "https://api.github.com/repos/{}/{}/commits".format(av[2], av[1]))
+    commit = new_request.json()
+    try:
+        for num in range(10):
+            print("{}: {}".format(
+                commit[num].get("sha"),
+                commit[num].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
