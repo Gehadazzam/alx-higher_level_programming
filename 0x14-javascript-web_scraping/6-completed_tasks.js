131d05d5c91632abf8191aab4064e459f9a99538
gehad <gehadgamalazzam@gmail.com>
2024-04-23 22:09:12 +0200

    Task 6 updated semiststander

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,16 +7,14 @@ request.get(url, (err, response, body) => {
   if (err) console.error(err);
   const all = JSON.parse(body);
   for (let p = 0; p < all.length; p++) {
-    const userId = all[p].userId;
     const completed = all[p].completed;
     if (completed) {
       if (tracker[all[p].userId] === undefined) {
         tracker[all[p].userId] = 1;
-      }
-      else {
+      } else {
         tracker[all[p].userId] += 1;
       }
+    }
   }
-}
   console.log(tracker);
 });
