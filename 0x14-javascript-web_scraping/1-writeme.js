bebf591b51857134b7dbd5c4fc62019180cbd699
gehad <gehadgamalazzam@gmail.com>
2024-04-23 15:30:31 +0200

    update task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const path = process.argv[2];
 const string = process.argv[3];
-fs.writeFile(path, string, 'utf-8', (err, data) => {
+fs.writeFile(path, string, 'utf-8', (err) => {
   if (err) console.error(err);
-  console.log(data);
 });
